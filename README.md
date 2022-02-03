97637ad7afb898dd05081fdb809bfff6888f11d1
Taiju Sanagi <taijusanagi@gmail.com>
2022-02-04 08:47:26 +0900

    chore: recovered

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # example-cryptomyname
-This is cryptomyname 
+
+This is cryptomyname recovered content
+
+## Contract source code
+
+https://ropsten.etherscan.io/address/0x23bffb7df704331785ce9a388d2c47aa95df21d2#code
---
+++ js/cryptomyname_abi.js
@@ -0,0 +1,91 @@
+var cryptoMyNameABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "checkNewAddress",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_userName",
+        type: "string",
+      },
+    ],
+    name: "checkNewUserName",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_userName",
+        type: "string",
+      },
+    ],
+    name: "getAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "getUserName",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_userName",
+        type: "string",
+      },
+    ],
+    name: "registerUser",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
